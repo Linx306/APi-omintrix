a86ce00320e3b0a67334dc8ae53b55af758b78a9
Linx306 <Hassam306@gmail.com>
2024-01-30 13:22:36 -0700

    Fs
    Se agrego el modificador del archivo JSON usando fs  para darle write al file y que permita al delete y al post modificar el archivo json

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express =require('express')
+const fs = require('fs');
 const server =express()
 const omnitrixData = require('./omnitrix.json');
 //get da todas las versiones del omnitrix
+server.use(express.json());
 server.get('/omnitrix', (req, res) => {
   res.json(omnitrixData.versions);
 });
@@ -17,13 +19,27 @@ server.get('/omnitrix/:id/aliens', (req, res) => {
   const version = omnitrixData.versions.find(x => x.id === id);
   res.json(version.aliens);
 })
-//delete un omnitrix 
+//delete omnitrix
 server.delete('/omnitrix/:id', (req, res) => {
   const id = parseInt(req.params.id);
   const indexToRemove = omnitrixData.versions.findIndex(x => x.id === id);
     omnitrixData.versions.splice(indexToRemove, 1);
+    fs.writeFile('./omnitrix.json', JSON.stringify(omnitrixData, null, 2), (err) => {
+    });
   res.json(omnitrixData.versions);
 });
+
+
+//agregar omnitrix
+server.post('/omnitrix', (req, res) => {
+  const newVersion = req.body;
+  omnitrixData.versions.push(newVersion);
+  fs.writeFile('./omnitrix.json', JSON.stringify(omnitrixData, null, 2), (err) => {
+  })
+  res.json(omnitrixData.versions);
+});
+
   server.listen(3002,()=>{
     console.log("SERVER IS RUNNING")
-    })
+    }) 
+
