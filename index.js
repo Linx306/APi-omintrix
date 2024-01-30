ba9dce02b546afaff88aefe45f32a9137e7b01d5
Linx306 <Hassam306@gmail.com>
2024-01-29 19:27:21 -0700

    Cambio de lugares
    El index asi como el omnitrix.json salen de la carpeta individual.json

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const express =require('express')
 const server =express()
 const omnitrixData = require('./omnitrix.json');
 
-server.get('/omnitrix', (req, res) => {
+server.get('/', (req, res) => {
     res.json(omnitrixData.versions);
   });
+
+  server.listen(3002,()=>{
+    console.log("SERVER IS RUNNING")
+    })
